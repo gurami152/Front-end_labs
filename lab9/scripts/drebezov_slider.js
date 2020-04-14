21c58502f76eb1b1db8a5739db664e1699fd4d05
gurami152 <gurami152@gmail.com>
2020-04-14 19:05:07 +0300

    delete npm jquery

--- lab9/scripts/drebezov_slider.js
+++ lab9/scripts/drebezov_slider.js
@@ -1,54 +1,38 @@
 var right = -40;
 var speed = 5;
-var buttonRight;
-var buttonLeft;
-document.addEventListener("DOMContentLoaded", function(event) {
-    var buttonRight = document.getElementById("right");
-    buttonRight.onclick = Right;
-    var buttonLeft = document.getElementById("left");
-    buttonLeft.onclick = Left;
-    speed1Button = document.getElementById("speed_1");
-    speed1Button.onclick=Speed_1;
-    speed2Button = document.getElementById("speed_2");
-    speed2Button.onclick=Speed_2;
-    speed3Button = document.getElementById("speed_3");
-    speed3Button.onclick=Speed_3;
-    speed=5;
+$(document).ready(function(){
+    $('#right').on('click',function(){
+    var photos = $('#slider_photos');
+        if (right!=-1492){
+            right = right - 242;
+        }
+        photos.css('left',right + 'px');
+        photos.css('transition', speed + 's');
+    });
+
+    $('#left').on('click',function(){
+        var photos = $('#slider_photos');
+        if (right!=-40){
+            right = right + 242;
+        }
+        photos.css('left', right + 'px');
+        photos.css('transition', speed + 's');
   });
   
-function Speed_1(){
-    speed=5*1;
-    console.log(speed);
-}
+  $('#speed_1').on('click',function(){
+    var photos = $('#slider_photos');
+    speed = 5*1;
+    photos.css('transition', speed + 's');
+  });
+  $('#speed_2').on('click',function(){
+    var photos = $('#slider_photos');
+    speed = 5*2;
+    photos.css('transition', speed + 's');
+  });
+  $('#speed_3').on('click',function(){
+    var photos = $('#slider_photos');
+    speed = 5*3;
+    photos.css('transition', speed + 's');
+  });
 
-
-function Speed_2(){
-    speed=5*2;
-    console.log(speed);
-}
-
-function Speed_3(){
-    speed=5*3;
-    console.log(speed);
-}
-
-function Right(){
-    var photos = document.getElementById('slider_photos');
-    if (right!=-1492){
-        right = right - 242;
-    }
- 
-    console.log(right.toString());
-    photos.style.transition = speed +'s';
-    photos.style.left = right +'px';
-}
-
-function Left(){
-    var photos = document.getElementById('slider_photos');
-    if (right!=-40){
-        right = right + 242;
-    }
-    console.log(right.toString());
-    photos.style.transition = speed +'s';
-    photos.style.left = right +'px';
-  }
+});
